1071283fd0ab521df1e496c2ea0f3b775e15c0f4
abdelrahman-gad <abdelrahmangad95@gmail.com>
2020-09-22 02:08:34 +0200

    deploy build directory

---
+++ build/precache-manifest.6a47af3dc6323551a93c3ec4283ed94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4356dd594e21b614942ad7cb0e057c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50acff6840045969ae79",
+    "url": "/static/css/main.2b2309a1.chunk.css"
+  },
+  {
+    "revision": "9d892c50a70500d46592",
+    "url": "/static/js/2.3143aeba.chunk.js"
+  },
+  {
+    "revision": "2197e867d978872afb824139f7f0a37a",
+    "url": "/static/js/2.3143aeba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50acff6840045969ae79",
+    "url": "/static/js/main.37d902f6.chunk.js"
+  },
+  {
+    "revision": "c62fef54687074e4c1b1",
+    "url": "/static/js/runtime-main.ccd2c2ce.js"
+  }
+]);
