fec5ddb14de72da04339405845f35c68851d0150
abdelrahman-gad <abdelrahmangad95@gmail.com>
2020-10-29 03:20:35 +0200

    Refactor Signin Page

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  , {Component} from 'react';
 import { connect } from 'react-redux';
 import {Redirect,NavLink} from 'react-router-dom';
 import {signIn } from '../../store/actions/authActions';
@@ -6,9 +6,9 @@ import {signIn } from '../../store/actions/authActions';
 
 
 
-class SignIn  extends React.Component{
+class SignIn  extends Component{
 
-  state={
+  state = {
     email:'',
     password:'',
     errors:{
@@ -17,7 +17,7 @@ class SignIn  extends React.Component{
     }
   }
   
- handleChange = (e) =>{
+ handleChange = e => {
   e.preventDefault();
   this.setState({
     [e.target.id]:e.target.value
@@ -25,71 +25,72 @@ class SignIn  extends React.Component{
 }
 
 
-handleSubmit = (e) => {
+handleSubmit = e => {
      e.preventDefault();
-    
-    if(this.state.email.length>0 && this.state.password.length>0   ) {
+    if(this.state.email.length > 0 && this.state.password.length > 0   ) {
       this.props.signIn(this.state);
     }
 }
 
+newForm = () => (
+  <form className="form" onSubmit={this.handleSubmit}>
+   
+    <div className="form-group">
+      <input 
+        type="email" 
+        placeholder="Email Address"   
+        id="email" 
+        onChange={this.handleChange} 
+        value={this.state.email}
+        
+      />
+    </div>
+    <div className="form-group">
+      <input 
+        type="password" 
+        placeholder="password"  
+        id="password" 
+        onChange={this.handleChange} 
+            
+      />
 
-render(){
-  const {authError , auth  } = this.props;
-  let alert =  authError ? <div className="alert alert-danger"> {authError} </div>:"";
+    </div>
+    <div className="form-group">
+      <input type="submit" className="btn btn-primary" value="Sign In" />
+    </div>
+  </form>
+)
+
+showError = authError => (authError ? <div className="alert alert-danger"> {authError} </div>:"") 
+showSignupLink = () => (<p className="my-1">Don't have an account ? <NavLink exact to="/signup"> Sign Up  </NavLink></p>)  
+
+render() {
+  const { authError , auth  } = this.props;
 //  console.log('from sign in component');
 //   console.log(auth.uid);
+ 
   if(authError || !auth.uid ){
   return (
     <section className="container">
+     {this.showError(authError)}
+     {this.newForm()}
+     {this.showSignupLink()}  
      
-    <form action="dashboard.html" className="form" onSubmit={this.handleSubmit}>
-      {alert}
-      <div className="form-group">
-        <input 
-          type="email" 
-          placeholder="Email Address"   
-          id="email" 
-          onChange={this.handleChange} 
-          value={this.state.email}
-          
-        />
-      </div>
-      <div className="form-group">
-        <input 
-           type="password" 
-           placeholder="password"  
-           id="password" 
-           onChange={this.handleChange} 
-               
-        />
-
-      </div>
-      <div className="form-group">
-        <input type="submit" className="btn btn-primary" value="Sign In" />
-      </div>
-    </form>
-    <p className="my-1">
-      Don't have an account ? <NavLink exact to="/signup"> Sign Up  </NavLink>
-    </p>
   </section>  
   );
 
-  }else if(auth.uid){ 
+  } else if(auth.uid){ 
     return <Redirect exact to="/dashboard" />
-  }
-   
- 
+  }  
+ }
 }
 
-}
-
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = dispatch => {
   return {
     signIn: (creds) => dispatch(signIn(creds))
   }
 }
-const mapStateToProps = (state)=>{
+const mapStateToProps = state => {
   const auth = state.firebase.auth;
       // console.log(state);
   return {
