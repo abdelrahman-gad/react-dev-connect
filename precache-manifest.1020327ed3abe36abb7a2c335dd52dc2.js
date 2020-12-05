f037063d6fd408d197c3f46ee09949279efaea50
abdelrahman-gad <abdelrahmangad95@gmail.com>
2020-12-05 15:51:11 +0200

    Updates

---
+++ precache-manifest.1020327ed3abe36abb7a2c335dd52dc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdc8e3511c2c94b69185aa02d2de159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b42177ed8106b01c4318",
+    "url": "/static/css/2.ce7663c7.chunk.css"
+  },
+  {
+    "revision": "b64bd1bd2c11109653e5",
+    "url": "/static/css/main.534529e7.chunk.css"
+  },
+  {
+    "revision": "b42177ed8106b01c4318",
+    "url": "/static/js/2.4d329f3a.chunk.js"
+  },
+  {
+    "revision": "d4af3bcb17c863c88054e9874901c70c",
+    "url": "/static/js/2.4d329f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64bd1bd2c11109653e5",
+    "url": "/static/js/main.ba0e0f1d.chunk.js"
+  },
+  {
+    "revision": "c62fef54687074e4c1b1",
+    "url": "/static/js/runtime-main.ccd2c2ce.js"
+  }
+]);
